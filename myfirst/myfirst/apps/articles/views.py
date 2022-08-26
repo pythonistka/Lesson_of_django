653dfd414dda536aa85eb8b4333d2cc75a321426
Lopatina Darya <baboleh@rambler.ru>
2022-08-26 23:42:37 +0300

    Связан шаблонизатор с ORM прописаны псевдонимы ссылок

--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -1,6 +1,31 @@
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 
+from django.urls import reverse
+
+from .models import Article, Comment
+
+
 def index(request):
-    return render(request, 'articles/list.html')
+    latest_articles_list = Article.objects.order_by('-pub_date')[:5]
+    return render(request, 'articles/list.html', {'latest_articles_list': latest_articles_list})
 
 
+def detail(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404("Статья не найдена!")
+
+    return render(request, 'articles/detail.html', {'article': a})
+
+
+def leave_comment(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404("Статья не найдена!")
+
+    a.comment_set.create(author_name = request.POST['name'], comment_text = request.POST['text'])
+
+    return HttpResponseRedirect(reverse('articles:detail', args = (a.id,)))
